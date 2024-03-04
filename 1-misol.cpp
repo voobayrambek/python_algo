5f20c278570cba4f0c543feb8977c3364764e05d
voobayrambek <161755934+voobayrambek@users.noreply.github.com>
2024-03-04 17:25:50 +0500

    1-misol
    amaliy ish

---
+++ 1-misol.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+double D(double q, double p){
+    return L=pow(q,p);
+}
+int main(){
+    double q,p;
+    cout<<"Sonni kiriting.";
+    cin>>q>>p;
+    double L=D(q,p);
+    cout<<L;
+}
